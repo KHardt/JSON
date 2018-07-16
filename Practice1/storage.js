71c667c502fae27a8914746f2b5963d2abc43602
Klaus Hardt <klausheina@gmail.com>
2018-07-16 14:50:29 -0500

    first commit

---
+++ Practice1/storage.js
@@ -0,0 +1,11 @@
+const HomeInventoryDatabase = {}
+
+HomeInventoryDatabase.furniture = []
+HomeInventoryDatabase.crafts = []
+HomeInventoryDatabase.electronics = []
+
+const antiqueSecretary = {
+    name: "Beidemieher Secretary",
+    location: "Mom's Basement",
+    description: "Belonged to Grandmother. Antique. Value unknown."
+  },
